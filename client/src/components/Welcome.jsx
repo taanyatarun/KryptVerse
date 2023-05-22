7198d074499ac1eb419e0fa766bff052797c6f2f
TaanyaTarun <taanyatarun@gmail.com>
2023-05-22 21:51:31 +0530

    second commit

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -95,7 +95,7 @@ const Welcome = () => {
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
             <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
             <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
-            {/* <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} /> */}
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
             <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
             <div className="h-[1px] w-full bg-gray-400 my-2"/>
